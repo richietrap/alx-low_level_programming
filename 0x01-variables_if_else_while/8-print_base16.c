484881b96f0800feaf1fc22bb78cd65710b11d0a
richietrap <dosumurichard@yahoo.com>
2022-12-09 18:42:24 -0800

    Fixed

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
@@ -13,11 +11,11 @@ int main(void)
 	int d;
 	char low;
 
-	for (d = '0'; d <= '9'; d++)
-		putchar(d);
+	for (d = '0'; d < '10'; d++)
+		putchar((d % 10) + '0');
 	for (low = 'a'; low <= 'f'; low++)
 		putchar(low);
-		putchar('\n');
+	putchar('\n');
 
 	return (0);
 }
