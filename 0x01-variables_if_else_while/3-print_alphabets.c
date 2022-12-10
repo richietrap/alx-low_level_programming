a7b4efc8d58e3cf2393d6373d6f64bfc7960b8ed
richietrap <dosumurichard@yahoo.com>
2022-12-09 18:24:32 -0800

    Task 3 done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,9 +11,12 @@ int main(void)
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
-	putchar(low);
+		putchar(low);
+	
 	for (low = 'A'; low <= 'Z'; low++)
 		putchar(low);
-		putchar('\n');
+	
+	putchar('\n');
+	
 	return (0);
 }
