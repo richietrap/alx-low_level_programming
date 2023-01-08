1d29de8d576bc38dca897cc3ede5c9c000c09327
richietrap <dosumurichard@yahoo.com>
2023-01-08 09:35:43 -0800

    Typos fixed

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,41 +10,30 @@
 
 int main(int argc, char **argv)
 {
-	int total, count;
-	unsigned int i;
-	char *p;
-	int cents[] = {25, 10, 5, 2};
+	int cents, ncoins = 0;
 
-	if (argc != 2)
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	total = strtol(argv[1], &p, 10);
-	count = 0;
+	cents = atoi(argv[1]);
 
-	if (!*p)
+	while (cents > 0)
 	{
-		while (total > 1)
-		{
-			for (i = 0; i < sizeof(cents[i]); i++)
-			{
-				if (total >= cents[i])
-				{
-					count += total / cents[i]);
-					total = total % cents[i];
-				}
-			}
-		}
-		if (total == 1)
-			count++;
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
 	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
-	printf("%d\n", count);
+	printf("%d\n", ncoins);
 	return (0);
 }
